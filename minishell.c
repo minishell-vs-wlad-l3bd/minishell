f489edadae6521d9a24cf466b14c2f5bac6ec3f7
Abdellah Ayad <aayadabdellah08@gmail.com>
2025-03-11 14:47:49 +0000

    hanldle SIGINT & SIGQUIT & add is_builtin function

--- minishell.c
+++ minishell.c
@@ -1,5 +1,23 @@
 #include "minishell.h"
 
+void handler(int sig)
+{
+	(void)sig;
+	if (sig == SIGINT)
+		write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+int is_builtin(char *str)
+{
+	if (!ft_strcmp(str, "echo") || !ft_strcmp(str, "cd")
+		|| !ft_strcmp(str, "pwd") || !ft_strcmp(str, "export")
+		|| !ft_strcmp(str, "unset") || !ft_strcmp(str, "env"))
+		return (1);
+	return (0);
+}
 pid_t excuve_fonc(char *path, char **alll)
 {
     pid_t i = fork();
@@ -13,14 +31,23 @@ int main(int ac, char **av, char **env)
 {
     char **paths;
     char **cmd;
+	 char *path_v;
     ac = 1;
     av = NULL;
     while(1)
     {
+		signal(SIGINT, handler);
+		signal(SIGQUIT, handler);
         paths = ft_split(find_path(env), ':');
         char *str = readline("\033[34mminishell\033[0m \033[31m▶ \033[0m");
+		if(!ft_strcmp(str, "exit") || str == NULL)
+			exit(1);
+		if (!str[0])
+			continue;
         cmd = ft_split(str, ' ');
-        char *path_v = find_cmd_path(paths, cmd[0]);
+        path_v = find_cmd_path(paths, cmd[0]);
+		if (!path_v)
+			return (1);
         pid_t n = excuve_fonc(path_v, cmd);
         add_history(str);
         waitpid(n, NULL, 0);
